7973dd8fbf03d65f2e2b2620ea61a22761085c39
D3RAH <emmanuelchideraa9@gmail.com>
2026-05-12 22:46:45 +0100

    initial Commit: Slicestack Premium Pizza

---
+++ src/HomeMenu.jsx
@@ -0,0 +1,31 @@
+
+export const HomeMenu =[
+
+    {
+        id:'1',
+        name:'Cheese Pizza',
+        price:'$5.99',
+        image:'https://kitchenatics.com/wp-content/uploads/2020/09/Cheese-pizza-1.jpg'
+
+    },
+    {
+        id:'2',
+        name:'Pepperoni Pizza',
+        price:'$2.99',
+        image:'https://www.jennycancook.com/wp-content/uploads/2013/02/PeppPizza_600.jpg'
+    },
+    {
+        id:'3',
+        name:'Strawberry Pizza',
+        price:'$2.99',
+        image:'https://www.shakentogetherlife.com/wp-content/uploads/2019/05/strawberry-dessert-pizza-cake-mix-featured.jpg'
+    },
+    {
+    id: '4',
+    name: 'Strawberry Pizza',
+    price: '$4.99',
+    image: 'https://www.shakentogetherlife.com/wp-content/uploads/2019/05/strawberry-dessert-pizza-cake-mix-featured.jpg'
+    },
+]
+
+export default HomeMenu
